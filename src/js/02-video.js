4b6ea98aba7b6960bf5d7642299e79870ad7f7e7
KaterynaNoskova <kateryna.noskova@gmail.com>
2023-07-25 20:28:57 +0200

    111

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Vimeo from "@vimeo/player";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+function onPlay(evt){
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(evt.seconds));
+};
+
+player.on('play', function() {
+    // iframe.ontimeupdate();
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+// if (localStorage.currentTime){
+//     iframe.currentTime = localStorage.currentTime
+// };
+// iframe.addEventListener('timeupdate', () => localStorage.currentTime = iframe.currentTime);
+// console.log(localStorage.currentTime);
+// //запись в локалку с временем
+// localStorage.setItem('videoplayer-current-time', JSON.stringify());
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
+const feedbackForm = document.querySelector('.feedback-form');
+const textMessage = document.querySelector('textarea');
+const ls_Key = 'feedback-form-state';
 
+updateOutput ();
+feedbackForm.addEventListener("submit", saveMessage);
+
+function saveMessage(evt){
+    evt.preventDefault();
+    localStorage.setItem(ls_Key, feedbackForm.elements.message.value);
+    updateOutput();
+    feedbackForm.reset();
+};
+function updateOutput() {
+    textMessage.textContent = localStorage.getItem(ls_Key) || "";
+}
