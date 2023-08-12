ea58515e50e7bdc974c6b6d001ce121af5a7476b
KaterynaNoskova <kateryna.noskova@gmail.com>
2023-08-12 16:51:46 +0200

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,15 @@
-import Vimeo from "@vimeo/player";
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo(iframe);
+const player = new Player(iframe);
 
-function onPlay(evt){
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(evt.seconds));
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data){
+    localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-player.on('play', function() {
-    // iframe.ontimeupdate();
-    console.log('played the video!');
-});
+const currentTime = localStorage.getItem('videoplayer-current-time') || 0;
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
-
-// if (localStorage.currentTime){
-//     iframe.currentTime = localStorage.currentTime
-// };
-// iframe.addEventListener('timeupdate', () => localStorage.currentTime = iframe.currentTime);
-// console.log(localStorage.currentTime);
-// //запись в локалку с временем
-// localStorage.setItem('videoplayer-current-time', JSON.stringify());
+player.setCurrentTime(currentTime);
