1affe45d4da19582601c1700af1c2f26d70d2226
KaterynaNoskova <88933282+KaterynaNoskova@users.noreply.github.com>
2023-08-12 15:19:56 +0000

    Deploying to gh-pages from @ KaterynaNoskova/goit-js-hw-08@625f57fe46cbcb0151db13f6a0efec83531298f6 🚀

--- 03-feedback.5abb2c87.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form"),t=document.querySelector("textarea");function a(){t.textContent=localStorage.getItem("feedback-form-state")||""}a(),e.addEventListener("submit",(function(t){t.preventDefault(),localStorage.setItem("feedback-form-state",e.elements.message.value),a(),e.reset()}));
-//# sourceMappingURL=03-feedback.5abb2c87.js.map
--- 03-feedback.d83e88c7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),t=document.querySelector("textarea"),a="feedback-form-state";function n(){t.textContent=localStorage.getItem(a)||""}n(),e.addEventListener("submit",(function(t){t.preventDefault(),localStorage.setItem(a,e.elements.message.value),n(),e.reset()}))}();
-//# sourceMappingURL=03-feedback.d83e88c7.js.map
