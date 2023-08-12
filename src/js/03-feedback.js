625f57fe46cbcb0151db13f6a0efec83531298f6
KaterynaNoskova <kateryna.noskova@gmail.com>
2023-08-12 17:19:26 +0200

    third task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,51 @@
+import throttle from "lodash.throttle";
+
 const feedbackForm = document.querySelector('.feedback-form');
-const textMessage = document.querySelector('textarea');
 const ls_Key = 'feedback-form-state';
+let currentData;
 
-updateOutput ();
-feedbackForm.addEventListener("submit", saveMessage);
+feedbackForm.addEventListener('input', throttle(onInput, 500));
 
-function saveMessage(evt){
-    evt.preventDefault();
-    localStorage.setItem(ls_Key, feedbackForm.elements.message.value);
-    updateOutput();
-    feedbackForm.reset();
+const currentLocalData = JSON.parse(localStorage.getItem(ls_Key));
+
+if(currentLocalData) {
+    feedbackForm.email.value = currentLocalData.email;
+    feedbackForm.message.value = currentLocalData.message;
+    currentData = {
+        email:feedbackForm.email.value,
+        message:feedbackForm.message.value,
+    };
 };
-function updateOutput() {
-    textMessage.textContent = localStorage.getItem(ls_Key) || "";
-}
+
+function onInput(){
+    currentData = {
+        email:feedbackForm.email.value,
+        message:feedbackForm.message.value,
+    };
+    localStorage.setItem(ls_Key, JSON.stringify(currentData));
+};
+
+feedbackForm.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    localStorage.removeItem(ls_Key);
+    resizeTo(feedbackForm);
+};
+
+// const feedbackForm = document.querySelector('.feedback-form');
+// const textMessage = document.querySelector('textarea');
+// const ls_Key = 'feedback-form-state';
+
+// updateOutput ();
+// feedbackForm.addEventListener("submit", saveMessage);
+
+// function saveMessage(evt){
+//     evt.preventDefault();
+//     localStorage.setItem(ls_Key, feedbackForm.elements.message.value);
+//     updateOutput();
+//     feedbackForm.reset();
+// };
+// function updateOutput() {
+//     textMessage.textContent = localStorage.getItem(ls_Key) || "";
+// }
