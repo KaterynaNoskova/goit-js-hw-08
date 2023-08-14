e580db8e1f003d54e101f7ea0000e178e74309b7
KaterynaNoskova <kateryna.noskova@gmail.com>
2023-08-14 19:52:20 +0200

    third new

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -30,22 +30,4 @@ feedbackForm.addEventListener('submit', onSubmit);
 function onSubmit(evt) {
     evt.preventDefault();
     localStorage.removeItem(ls_Key);
-    resizeTo(feedbackForm);
 };
-
-// const feedbackForm = document.querySelector('.feedback-form');
-// const textMessage = document.querySelector('textarea');
-// const ls_Key = 'feedback-form-state';
-
-// updateOutput ();
-// feedbackForm.addEventListener("submit", saveMessage);
-
-// function saveMessage(evt){
-//     evt.preventDefault();
-//     localStorage.setItem(ls_Key, feedbackForm.elements.message.value);
-//     updateOutput();
-//     feedbackForm.reset();
-// };
-// function updateOutput() {
-//     textMessage.textContent = localStorage.getItem(ls_Key) || "";
-// }
